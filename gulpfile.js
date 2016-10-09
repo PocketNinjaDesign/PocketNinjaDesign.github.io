e2d79b34200da3d894355c3a0a1b96bd175db9d2
Eyeamaman <eyeamaman@gmail.com>
2016-10-10 00:47:59 +0100

    Added sass, iconfont

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,63 @@
+var async = require('async');
 var gulp = require('gulp');
+var lodash = require('lodash');
 
-gulp.task('default', function() {
+var iconfont = require('gulp-iconfont');
+var consolidate = require('gulp-consolidate');
+var rename = require("gulp-rename");
+
+var sass = require('gulp-sass');
+
+
+/*
+ *  STYLING
+ *
+ */
+gulp.task('style', function () {
+  return gulp.src('./dev/assets/scss/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./assets/css'));
+});
+
+gulp.task('sass:watch', function () {
+  gulp.watch('./dev/assets/scss/**/*.scss', ['sass']);
+});
+
+
+
+/*
+ * ICON FONT
+ *
+ */
+gulp.task('Iconfont', function(done){
+  var iconStream = gulp.src(['./dev/assets/icons/*.svg'])
+        .pipe(iconfont({ fontName: 'pnfont' }));
+
+  async.parallel([
+    function handleGlyphs (cb) {
+      iconStream.on('glyphs', function(glyphs, options) {
+        gulp.src('./dev/assets/templates/_icons.txt')
+          .pipe(consolidate('lodash', {
+            glyphs: glyphs,
+            fontName: 'pnfont',
+            fontPath: './dev/assets/fonts/',
+            className: 'if'
+          }))
+          .pipe(rename("_fonts.scss"))
+          .pipe(gulp.dest('./dev/assets/scss/'))
+          .on('finish', cb);
+      });
+    },
+    function handleFonts (cb) {
+      iconStream
+        .pipe(gulp.dest('./assets/fonts/'))
+        .on('finish', cb);
+    }
+  ], done);
+});
+
+
+
+gulp.task('default', ['Iconfont', 'style'], function() {
   // Run some shizzle in here when the time is right!
 });
