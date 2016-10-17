06e924482019e51a3032f7844db79d1e413db542
Eyeamaman <eyeamaman@gmail.com>
2016-10-17 23:32:18 +0100

    Latest changes to the home page

--- dev/assets/js/ninjules/page/home.js
+++ dev/assets/js/ninjules/page/home.js
@@ -3,17 +3,17 @@
 Yo.add('page.home', function() {
   if($('#pageHome').length > 0) {
     var data = {
-      // A labels array that can contain any sort of values
-      labels: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri'],
-      // Our series array that contains series objects or in this case series data arrays
       series: [
-        [5, 2, 4, 2, 0]
+        90, 180, 90
       ]
     };
 
-    // Create a new line chart object where as first parameter we pass in a selector
-    // that is resolving to our chart container element. The Second parameter
-    // is the actual data object.
-    new Chartist.Line('.ct-chart', data);
+    new Chartist.Pie('.ct-chart', data, {
+      donut: true,
+      donutWidth: 100,
+      startAngle: 0,
+      total: 360,
+      showLabel: false
+    });
   }
 });
