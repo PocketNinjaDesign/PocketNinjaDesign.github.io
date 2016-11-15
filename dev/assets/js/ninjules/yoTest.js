0fe1575274aab17c261c44ed7bb7b79f139233f0
PocketNinjaCoUk <eyeamaman@gmail.com>
2016-11-15 19:25:04 +0000

    change mobile to tablet to see if things change

--- dev/assets/js/ninjules/yoTest.js
+++ dev/assets/js/ninjules/yoTest.js
@@ -6,7 +6,8 @@ Yo.add('yoTest', ['config.breakpoints'], function(configBreakPoints) {
   if ($('#yoTest').length > 0) {
     yoTest = $('#yoTest');
     yoTest.find('h2').html('Yo and jQuery working');
-    yoTest.find('p.breakpoint').html('Also the configuration shared variables should be working as well now.  The size for mobile landscape is ' + configBreakPoints.mobileLandscape);
+    yoTest.find('p.breakpoint').html('Also the configuration shared variables should be working as well now.  The' +
+      ' size for tablet landscape is ' + configBreakPoints.tabletLandscape);
     yoTest.find('p.colour').html('The colours are also working because this box actually uses colours pulled from the shared config.');
   }
 
